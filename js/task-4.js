405f98d3237b57e077318d5227f5ca319d8673f8
denis-cactus <m.play.sys@gmail.com>
2025-01-22 12:34:32 +0200

    update

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const email = form.elements.email;
+  const password = form.elements.password;
 
+  if (email.value.trim() === '' || password.value.trim() === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+  const formData = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+  console.log(formData);
+  form.reset();
+});
