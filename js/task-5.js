70300f3e76591460d178d46ed0c130cb4297a475
denis-cactus <m.play.sys@gmail.com>
2025-01-22 13:17:34 +0200

    update

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,14 @@
+const buttonChangeColor = document.querySelector('.change-color');
+const spanChangeColor = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+buttonChangeColor.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  spanChangeColor.textContent = newColor;
+});
